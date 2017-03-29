04dd52197a336839ad83faeb2d88180e1529c326
Vasilii-Moskvin <vasiliy.moscvin@yandex.ru>
2017-03-29 09:56:07 +0300

    Update pep8_me.py
    - Замена x на _
    - Изменение имени namef на name_f

--- lesson_1/homework/pep8_me.py
+++ lesson_1/homework/pep8_me.py
@@ -2,27 +2,27 @@ import random
 import string
 
 
-def create_file(namef, dir, size):
+def create_file(name_f, dir, size):
     if not size.isdigit():
         if size.endswith('KB'):
             s1 = size.split('KB')
             size1 = int(s1[0]) * 1024
             token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) 
-                            for x in range(size1))
+                            for _ in range(size1))
         elif size.endswith('MB'):
             s1 = size.split('MB')
             size1 = int(s1[0]) * 1048567
             token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) 
-                            for x in range(size1))
+                            for _ in range(size1))
         elif size.endswith('GB'):
             s1 = size.split('GB')
             size1 = int(s1[0]) * 1073741824
             token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) 
-                            for x in range(size1))
+                            for _ in range(size1))
     else:
         token = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) 
-                        for x in range(int(size)))
-    with open(dir + namef, "w") as file:  # По-хорошшему, здесь нужно использовать os.path.join(dirm, namef)
+                        for _ in range(int(size)))
+    with open(dir + name_f, "w") as file:  # По-хорошшему, здесь нужно использовать os.path.join(dirm, namef)
         file.write(token)
 
 
